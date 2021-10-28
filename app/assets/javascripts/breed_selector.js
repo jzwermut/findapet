5715e8a6e19ec11c9b280f6d4371f39f654d39fd
Jonathan Wermut <jonathanwermut@Jonathans-MBP.modem>
2021-10-28 17:22:25 +1100

    changed config environments file production.rb to include uglifier harmony is true

--- app/assets/javascripts/breed_selector.js
+++ app/assets/javascripts/breed_selector.js
@@ -1,10 +1,10 @@
 $(document).ready (function() {
 
-	var breeds = $('#listing_breed_id').html();
+	const breeds = $('#listing_breed_id').html();
 	$('#listing_breed_id').empty();
 	$('#listing_breeder_id').change(function() {
-		var breeder = $('#listing_breeder_id option:selected').text();
-		var options = $(breeds).filter(`optgroup[label="${breeder}"]`).html();
+		let breeder = $('#listing_breeder_id option:selected').text();
+		let options = $(breeds).filter(`optgroup[label="${breeder}"]`).html();
 		if (options.length > 0) {
 			$('#listing_breed_id').html(options);
 		}
