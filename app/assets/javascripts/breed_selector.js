6062ce3e43a21db60e4f677a1095e04988e7d1fc
Jonathan Wermut <jonathanwermut@Jonathans-MBP.modem>
2021-10-28 17:18:30 +1100

    Changed const to var everywhere

--- app/assets/javascripts/breed_selector.js
+++ app/assets/javascripts/breed_selector.js
@@ -3,8 +3,8 @@ $(document).ready (function() {
 	var breeds = $('#listing_breed_id').html();
 	$('#listing_breed_id').empty();
 	$('#listing_breeder_id').change(function() {
-		let breeder = $('#listing_breeder_id option:selected').text();
-		let options = $(breeds).filter(`optgroup[label="${breeder}"]`).html();
+		var breeder = $('#listing_breeder_id option:selected').text();
+		var options = $(breeds).filter(`optgroup[label="${breeder}"]`).html();
 		if (options.length > 0) {
 			$('#listing_breed_id').html(options);
 		}
